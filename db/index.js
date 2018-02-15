410d7efe3c4836317d267d60ca2c9f2d276fd8c7
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-02-15 18:00:08 -0500

    nothing really

--- db/index.js
+++ db/index.js
@@ -48,7 +48,7 @@ const Contact = _conn.define('contact', {
     },
     formattedPhone: function() {
       let phone = this.number
-      phone = `(${phone[0]}${phone[1]}${phone[2]})-${phone[3]}${phone[4]}${phone[5]}-${phone[6]}${phone[7]}${phone[8]}${phone[9]}`
+      phone = `(${phone[0]}${phone[1]}${phone[2]}) ${phone[3]}${phone[4]}${phone[5]}-${phone[6]}${phone[7]}${phone[8]}${phone[9]}`
       return phone
     }
   }
